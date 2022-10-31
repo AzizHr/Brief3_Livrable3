2a45ef96240f1bf1c25723c095e1071d2c932645
AzizHr <azizharkati20@gmail.com>
2022-10-31 15:38:57 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,10 +3,5 @@ const nav_menu_icon = document.getElementById('nav_menu_icon');
 const nav = document.getElementById('nav');
 
 nav_menu_icon.addEventListener('click' , function(){
-    if(nav_menu.style.display == 'none'){
-        nav_menu.style.display = 'block';
-    }
-    else{
-        nav_menu.style.display = 'none';
-    }
+    nav_menu.classList.toggle("show")
 });
